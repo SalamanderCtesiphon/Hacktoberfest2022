a62318e62602792cfb8ad16f4330507d11b7cf6d
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-31 02:42:06 -0500

    thanks

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2045,6 +2045,13 @@ contributors = [
     id:421,
     fullname:"Gautami SInha",
     username:"https://github.com/amnotcreative",
-  }
+  },
+
+  {
+    id: 422,
+    fullname: "Siddharth",
+    username: "https://github.com/SalamanderCtesphon",
+  },
+
 
 ];
